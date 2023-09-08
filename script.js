90a4644a67797df8228175ce7619202322d5ca3b
Isapedro <isa.pedro92@hotmail.com>
2023-09-08 23:23:09 +0200

    fixed mistakes

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ function updateCity(event) {
   let citiesElement=document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city">
           <div>
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
-          <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small>")</div>
+          <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
         </div>`;
 }
 
