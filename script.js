6391df066162b26103427bdfba6e2a34658b3db2
Isapedro <isa.pedro92@hotmail.com>
2023-09-09 00:07:13 +0200

    added another city

--- script.js
+++ script.js
@@ -21,6 +21,17 @@ function updateTime() {
       "h:mm:ss[<small>]A[</small]"
     );
   }
+ let berlinElement = document.querySelector("#berlin");
+ if (berlinElement) {
+   let berlinDateElement = berlinElement.querySelector(".date");
+   let berlinTimeElement = berlinElement.querySelector(".time");
+   let berlinTime = moment().tz("Europe/Berlin");
+
+   berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+   berlinTimeElement.innerHTML = berlinTime.format(
+     "h:mm:ss[<small>]A[</small]"
+   );
+ }
 
   let tokyoElement = document.querySelector("#tokyo");
   if (tokyoElement) {
